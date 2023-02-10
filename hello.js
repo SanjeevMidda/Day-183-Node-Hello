d20e9d636b754ea9891c6a68fcbaa770ca6f679b
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2023-02-10 12:59:46 +0000

    Add files via upload
    Uploading source files.

---
+++ hello.js
@@ -0,0 +1,10 @@
+//1
+const myName = "Tim";
+//2
+console.log(`Hello ${myName}`);
+
+/*
+    Thinking
+        1. Created a variable named "myName" and stored the name of a person inside. 
+        2. Output this to the console.
+*/
